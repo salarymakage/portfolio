88b8e473c8f127e6937034533fd2fc7951e2d949
salarymakage <chesthareahtan@gmail.com>
2025-07-16 15:05:27 +0700

    fix run build

--- app/datascience/page.tsx
+++ app/datascience/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from 'next/image';
-import Link from 'next/link';
 import { useEffect, useRef } from 'react';
 
 // Animated Background Component inspired by the CodePen
